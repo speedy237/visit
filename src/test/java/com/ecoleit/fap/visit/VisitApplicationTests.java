567b61cc6d209d9aca0ec05b5bb3d5faed95938a
teyou1999 <yiyuemej@yahoo.fr>
2023-05-10 22:49:15 +0200

    initialize project

---
+++ src/test/java/com/ecoleit/fap/visit/VisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoleit.fap.visit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
